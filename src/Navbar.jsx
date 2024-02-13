df3cc1e8b8b1f66d572ac0fb162aa88c412b194c
ThunderBoltCodes <94512886+azimbijapur786@users.noreply.github.com>
2024-02-13 16:38:14 +0530

    navbar adjusted

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
-        <div className="navbar p-3 w-1/2 flex items-center justify-between">
+        <div className="navbar p-3 sm:w-1/2 flex items-center justify-between">
             <h1 className="text-2xl font-bold">Exercise Tracker</h1>
             <ul className="flex items-center p-3 gap-6 text-xl">
                 <Link to='/'>Home</Link>

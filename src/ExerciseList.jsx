1f819bbdb2d62b193a6f862efa536643543cbc1f
ThunderBoltCodes <94512886+azimbijapur786@users.noreply.github.com>
2024-02-13 16:32:42 +0530

    pagination added

--- src/ExerciseList.jsx
+++ src/ExerciseList.jsx
@@ -1,19 +1,24 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const ExerciseList = () => {
     const [workouts, setWorkouts] = useState([]);
     const [error, setError] = useState(false);
     const [isLoading, setLoading] = useState(false);
+    const [page, setPage] = useState(1); // Current page
+    const [limit, setLimit] = useState(5); // Number of workouts per page
+    const [totalPages, setTotalPages] = useState(0); 
 
     useEffect(() => {
         setError(false);
         setLoading(true);
-        axios.get('https://backend-exercise-tracker-8bci.onrender.com/')
+        axios.get(`https://backend-exercise-tracker-8bci.onrender.com/?page=${page}&limit=${limit}`)
             .then(function (response) {
                 console.log(response.data);
-                setWorkouts(response.data);
+                setWorkouts(response.data.workouts);
+                setTotalPages(response.data.totalPages);
+                console.log(totalPages);
                 setLoading(false);
                 setError(false);
             })
@@ -22,9 +27,9 @@ const ExerciseList = () => {
                 setLoading(false);
                 setError(true);
             });
-    }, []);
+    }, [page, limit]); // Add page and limit as dependencies
 
-    const handleDate = (date)=>{
+    const handleDate = (date) => {
         return new Date(date).toDateString();
     }
 
@@ -33,13 +38,19 @@ const ExerciseList = () => {
             {workouts && !error && !isLoading && workouts.map((work) => (
                 <div key={work._id} className="work p-6  rounded bg-green-100 cursor-pointer mt-4" >
                     <Link to={`/exercise/${work._id}`}>
-                    <div className="date text-lg font-bold">{handleDate(work.date)}</div>
-                    <div className="duration mt-2">{work.duration}</div>
+                        <div className="date text-lg font-bold">{handleDate(work.date)}</div>
+                        <div className="duration mt-2">{work.duration}</div>
                     </Link>
                 </div>
             ))}
             {isLoading && <div className='text-lg'>Loading...</div>}
             {error && <div className="text-lg text-red-500">Oops! Something went wrong!</div>}
+            {/* Pagination controls */}
+            <div className="pagination mt-4 flex justify-center text-lg">
+                {page>1 && <button className='' onClick={() => setPage(page - 1)} disabled={page === 1}>Previous</button>}
+                <span className="mx-4">Page {page}</span>
+                {page<totalPages && <button onClick={() => setPage(page + 1)}>Next</button>}
+            </div>
         </div>
     );
 }
